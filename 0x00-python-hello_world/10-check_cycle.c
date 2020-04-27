bf4594e7defa753ffad812969e26ec6c4dde8190
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-04-27 17:52:39 -0500

    add

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,12 +11,11 @@ int check_cycle(listint_t *list)
 
 	if (list == NULL)
 		return (0);
-	while (current != NULL && cisfun->next != NULL)
+	while (current != NULL && cisfun !=NULL && cisfun->next != NULL)
 	{
 		cisfun = cisfun->next;
 		current = current->next->next;
-
-		if (current == cisfun)
+		if (cisfun == current)
 			return (1);
 	}
 	return (0);

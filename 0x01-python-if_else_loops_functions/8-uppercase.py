534f918c758096b3d5951a42f5e73f9e986d6292
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-04-28 18:20:03 -0500

    checker is fun

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    str_upper = []
+    for idx in range(len(str)):
+        if ord('a') <= ord(str[idx]) <= ord('z'):
+            str_upper.append(chr(ord(str[idx]) - 32))
+        else:
+            str_upper.append(str[idx])
+
+    print("{}".format(''.join(str_upper)))

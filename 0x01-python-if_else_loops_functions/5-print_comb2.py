3c0be33db29c3eabda7dfd695d93059db062ef97
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-04-28 16:13:14 -0500

    checker is fuck

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for idx in range(0, 100):
     if idx != 99:
-        print("{:d}".format(idx).zfill(2), end=', ')
+        print("{:02d}".format(idx), end=', ')
     else:
         print("{:d}".format(idx))

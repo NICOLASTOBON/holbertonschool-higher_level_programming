b57076bbdb937f11752434b85f5318a7f734e17b
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-04-30 13:55:07 -0500

    imports all functions from the file calculator_1.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,6 @@
 import sys
 from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    op = ['+', '-', '*', '/']
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -10,15 +9,14 @@ if __name__ == '__main__':
         a = sys.argv[1]
         c = sys.argv[2]
         b = sys.argv[3]
-        if c in op:
-            if c is op[0]:
-                print("{} {} {} = {}".format(a, op[0], b, add(int(a), int(b))))
-            elif c is op[1]:
-                print("{} {} {} = {}".format(a, op[1], b, sub(int(a), int(b))))
-            elif c is op[2]:
-                print("{} {} {} = {}".format(a, op[2], b, mul(int(a), int(b))))
-            else:
-                print("{} {} {} = {}".format(a, op[3], b, div(int(a), int(b))))
+        if c is '+':
+            print("{} + {} = {}".format(a, b, add(int(a), int(b))))
+        elif c is '-':
+            print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
+        elif c is '*':
+            print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
+        elif c is '/':
+            print("{} / {} = {}".format(a, b, div(int(a), int(b))))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)

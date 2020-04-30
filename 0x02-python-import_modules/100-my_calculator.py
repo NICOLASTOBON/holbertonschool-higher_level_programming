c91592d6952bb36aa19f9917212915f2af5919a8
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-04-30 14:53:02 -0500

    imports all functions from the file calculator_1.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
+    import sys
+    from calculator_1 import add, sub, mul, div
     len_argv = len(sys.argv) - 1
     if len_argv != 3:
         print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))

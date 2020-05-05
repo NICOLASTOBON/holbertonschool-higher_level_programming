8027119af751670898b7417fea11584d64311b74
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-05 12:34:35 -0500

    removes all characters

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    if my_string:
-        return my_string.translate({ord(i): None for i in 'cC'})
+    my_new = my_string
+    return my_new.translate({ord(i): None for i in 'cC'})

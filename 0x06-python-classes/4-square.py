546b708b6b07053acfc4921b240383e6fc6fcad5
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-19 16:00:14 -0500

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
+"""Square class"""
 
 
-class Square:
+class Square():
+    """constuctor method"""
 
     def __init__(self, size=0):
+        """private attribute"""
+
         self.__size = size
 
     def area(self):
+        """Area of a square"""
+
         return self.__size ** 2
 
     @property
     def size(self):
+        """using property decorator"""
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Setter and value validation"""
+
         if isinstance(value, int) is False:
             raise TypeError("size must be an integer")
         elif value < 0:

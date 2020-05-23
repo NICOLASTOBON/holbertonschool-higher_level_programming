856dd903bdee2956f8e5a8f7b51e5ec66972e59a
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-23 01:07:33 -0500

    write unittests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,7 +9,7 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Test for integers"""
 
-    def test_max_integer(self):
+    def test_cases(self):
         """Cases"""
         self.assertEqual(max_integer([6, 7, 9]), 9)
         self.assertEqual(max_integer([13, -2, -1, -9]), 13)

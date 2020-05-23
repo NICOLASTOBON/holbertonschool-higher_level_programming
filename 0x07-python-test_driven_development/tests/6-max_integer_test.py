93c55c364a64714b6184c6f34757244e04e25891
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-22 21:00:05 -0500

    write unittests

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ test integers """
+
+    def max_integer_test(self):
+        """ test integers """
+
+        self.assertEqual(max_integer(['a', 'z', 'c']), 'z')
+        self.assertEqual(max_integer([1, 9, 8, 0]), 9)
+        self.assertEqual(max_integer([11, -9, 8, 0]), 11)
+        self.assertEqual(max_integer([-1, -9, -8, 0]), 0)
+        self.assertEqual(max_integer([1, 9, 22, 0]), 22)
+        self.assertEqual(max_integer([1, 9, 8, 10]), 10)
+        self.assertEqual(max_integer([1.2, 9.2, 3, 0]), 9.2)
+        self.assertEqual(max_integer(), None)
+
+
+if __name__ == "__main__":
+    unittest.main()

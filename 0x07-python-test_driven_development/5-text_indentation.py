6b6a09bc8baa266429ce157c7d964c74f800f32e
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-05-22 20:56:41 -0500

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -18,12 +18,12 @@ def text_indentation(text):
             continue
         else:
             new_text += i
-        
+
         if i == '.' or i == '?' or i == ':':
             new_text = new_text.strip(" ")
             print("{}".format(new_text), end="")
             print("\n")
             new_text = ""
-    
+
     new_text = new_text.strip(" ")
     print("{}".format(new_text), end="")

853500348dd68a7121a8d8416fcb085006e802dd
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-02 16:51:57 -0500

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class BaseGeometry """
+
+
+class BaseGeometry:
+
+    def area(self):
+        """ Public instance method """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validator integer """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

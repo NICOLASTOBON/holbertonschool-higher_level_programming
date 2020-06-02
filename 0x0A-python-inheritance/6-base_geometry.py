5c41f231be44c909400e7746a4b7866bd39534c9
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-02 15:58:21 -0500

    Public instance method area()

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class BaseGeometry """
+
+
+class BaseGeometry:
+
+    def area(self):
+        """ Public instance method """
+        raise Exception("area() is not implemented")

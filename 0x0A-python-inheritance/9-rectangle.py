3a0111d4840bd46c4a22368856ed2d8a047dee2b
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-02 18:28:41 -0500

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 """ class Rectangle """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """ class Rectangle """
 
     def __init__(self, width, height):
         """ initial attributes """

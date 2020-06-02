e2cf1bf41981322eca7dab09491208f28dd2298a
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-02 18:29:37 -0500

    square 1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
-
 """ Square #1 """
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ Square #1 """
 
     def __init__(self, size):
         """ initial attributes """

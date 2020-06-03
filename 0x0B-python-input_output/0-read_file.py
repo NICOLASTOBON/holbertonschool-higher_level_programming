04092796d4e058b2594049b8179802fb233dce65
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-03 16:57:58 -0500

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,5 @@
 def read_file(filename=""):
     """ Read a file """
     with open(filename, encoding="utf-8") as f:
-        line = f.read()
-    print(line, end="")
+        print(f.read())
     f.close()

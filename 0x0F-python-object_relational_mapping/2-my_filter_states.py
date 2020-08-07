8bffa36178bab3fb8d9cce9baddf2250e85629e7
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-07 01:59:32 -0500

    [add] script that takes in an argument and displays all values in the states table

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# script that takes in an argument and displays all values in the states table
+# where name matches the argument.
+
+import MySQLdb as _mysql
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name_db = sys.argv[3]
+    state_name = sys.argv[4]
+
+    my_db = _mysql.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=name_db
+    )
+
+    cur = my_db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name REGEXP '{}' ORDER BY id ASC;"
+        .format(state_name)
+        )
+
+    for state in cur.fetchall():
+        print(state)
+
+    cur.close
+    my_db.close

e9baf9c825e76086488d723e791362d3e604fefb
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-07 01:19:03 -0500

    [add]script that lists all states with a name starting with N (upper N)

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+
+import MySQLdb as _mysql
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name_db = sys.argv[3]
+
+    my_db = _mysql.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=name_db,
+        charset="utf8"
+    )
+
+    cur = my_db.cursor()
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC;")
+    for state in cur.fetchall():
+        print(state)
+    cur.close
+    my_db.close

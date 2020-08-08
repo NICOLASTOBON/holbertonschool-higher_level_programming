3d1750dddbdb522609ed1db554cba1e4057605a4
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-08 10:18:44 -0500

    [add]script that lists all states with a name starting with N (upper N)

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# script that lists all states with a name starting with N (upper N)
+""" script that lists all states with a name starting with N (upper N)"""
 
 import MySQLdb as _mysql
 import sys

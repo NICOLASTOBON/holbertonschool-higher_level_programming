ccbcf0aac5f88e2ab344abd836456534db8f0249
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-08 10:58:26 -0500

    [fix] add comments to the files

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that prints the first State object from the database hbtn_0e_6_usa """
+""" script that prints the first State object
+from the database hbtn_0e_6_usa """
 
 from model_state import Base, State
 from sqlalchemy import create_engine

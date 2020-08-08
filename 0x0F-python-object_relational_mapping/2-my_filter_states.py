1223ceb51d7095c22288a0216330b49b2c15fc6c
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-08 10:43:34 -0500

    [add] script that takes in an argument and displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
 
     cur = my_db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name='{}' ORDER BY id ASC;"
+        """SELECT * FROM states
+        WHERE BINARY name='{}' ORDER BY id ASC;"""
         .format(state_name)
         )
 

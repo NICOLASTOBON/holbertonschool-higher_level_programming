37e13f9a8cae31328de2a19dd025756c75f9f200
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-08 10:32:46 -0500

    [add] script that takes in an argument and displays all values in the states table

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# script that takes in an argument and displays all values in the states table
-# where name matches the argument.
+"""script that takes in an argument and displays all values in the states table
+where name matches the argument."""
 
 import MySQLdb as _mysql
 import sys

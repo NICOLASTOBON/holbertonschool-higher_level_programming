41ac2eddc6c2fb07e3799cceba330d9b588e7b76
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-09 20:16:13 -0500

    [add] script that lists all State objects

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -8,21 +8,21 @@ from sqlalchemy.orm import sessionmaker
 import sys
 
 if __name__ == "__main__":
-	username = sys.argv[1]
-	password = sys.argv[2]
-	name_db = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name_db = sys.argv[3]
 
-	engine = create_engine(
-		'mysql+mysqldb://{}:{}@localhost:3306/{}'
-		.format(username, password, name_db),
-		pool_pre_ping=True
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, name_db),
+        pool_pre_ping=True
     )
-	Base.metadata.create_all(engine)
-	Session = sessionmaker(bind=engine)
-	session = Session()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-	sts = session.query(State).order_by(State.id).all()
-	for st in sts:
-		print('{}: {}'.format(st.id, st.name))
-		for ct in st.cities:
-			print('	{}: {}'.format(ct.id, ct.name))
+    sts = session.query(State).order_by(State.id).all()
+    for st in sts:
+        print('{}: {}'.format(st.id, st.name))
+        for ct in st.cities:
+            print('	{}: {}'.format(ct.id, ct.name))

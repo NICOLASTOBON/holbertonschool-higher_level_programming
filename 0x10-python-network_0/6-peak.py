70c9a21b50229cd70fed7a4b205a4919661bb954
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-18 20:05:50 -0500

    cachama powers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,4 @@ def find_peak(list_of_integers):
     """
     This function returns the peak of a list
     """
-    if list_of_integers != []:
-        list_of_integers.sort()
-        return list_of_integers[-1]
+    return max(list_of_integers) if list_of_integers != [] else None

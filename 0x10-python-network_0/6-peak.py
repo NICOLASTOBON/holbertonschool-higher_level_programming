8e729cf3c80ae43b9a4d1a66379a68d5d92f26d7
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-18 19:14:24 -0500

    cachama powers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,5 +8,4 @@ def find_peak(list_of_integers):
     """
     if list_of_integers == []:
         return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+    return max(list_of_integers)

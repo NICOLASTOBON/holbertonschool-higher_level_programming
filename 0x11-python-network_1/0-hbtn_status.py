acdbd15e3f030b25677c1a2dcec5c6e60eaefa09
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-24 12:13:10 -0500

    [add] Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         status = response.read()
         print('Body response:')
-        print('\t - type: {}'.format(type(status)))
-        print('\t - content: {}'.format(status))
-        print('\t - utf8 content: {}'.format(status.decode('utf8')))
+        print('\t- type: {}'.format(type(status)))
+        print('\t- content: {}'.format(status))
+        print('\t- utf8 content: {}'.format(status.decode('utf8')))

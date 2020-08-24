df98c330ad89f57811400fe9e7ec0a0a84f0365d
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-24 15:49:30 -0500

    [add] takes in a letter and sends a POST request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     if sys.argv[1] is None:
         data['q'] = sys.argv[1]
     res = requests.post('http://0.0.0.0:5000/search_user', data)
-    if 'application/json' == res.headers.get('content-type'):
+    if 'json' not in res.headers.get('content-type'):
         if res.json():
-            print('[{}] {}'.format(res.json().get('id'), res.json().get('name')))
+            print('[{}] {}'.format(
+                res.json().get('id'), res.json().get('name')))
         else:
             print('No result')
     else:

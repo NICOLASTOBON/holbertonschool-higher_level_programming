a180ec396c4992b89a8c57300ca1b546bce5079c
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-08-24 15:44:17 -0500

    [add] takes in a letter and sends a POST request

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" takes in a letter and sends a POST request """
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    data = {'q': ''}
+    if sys.argv[1] is None:
+        data['q'] = sys.argv[1]
+    res = requests.post('http://0.0.0.0:5000/search_user', data)
+    if 'application/json' == res.headers.get('content-type'):
+        if res.json():
+            print('[{}] {}'.format(res.json()['id'], res.json()['name']))
+        else:
+            print('No result')
+    else:
+        print('Not a valid JSON')

6c95000f0f2314e1bcb437567fcfd5b94e25150f
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-15 13:04:01 -0500

    [add] arguments value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
 
-const value = process.argv.length;
-
-if (value < 3) {
-  console.log('No argument');
-} else {
-  for (let i = 2; i < value; i++) {
-    console.log(process.argv[i]);
-  }
-}
+console.log(process.argv[2] ? process.argv[2] : 'No argument');

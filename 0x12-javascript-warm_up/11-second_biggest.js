8a3f16d56ace154f534a365d255c1c7ebda49873
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-16 19:59:49 -0500

    [add] add

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@ const value = process.argv;
 if (value.length <= 3) {
   console.log(0);
 } else {
-  const newArr = value.slice(2).sort();
+  const newArr = value.slice(2).sort((a, b) => a - b);
   console.log(newArr[newArr.length - 2]);
 }

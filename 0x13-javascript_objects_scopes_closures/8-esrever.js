ca08345607beccbeb255ac498cdaea5d5d55903a
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-21 12:10:31 -0500

    [add] returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    for (let i = 0; i < list.length / 2; i++) {
-        let val = list[i];
-        list[i] = list[list.length - 1 - i];     
-        list[list.length - 1 - i] = val;
-    }
-
-    return list;
-}
+  for (let i = 0; i < list.length / 2; i++) {
+    const val = list[i];
+    list[i] = list[list.length - 1 - i];
+    list[list.length - 1 - i] = val;
+  }
+  return list;
+};

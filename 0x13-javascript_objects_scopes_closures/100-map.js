4df3bfd0b51a59cfd51f074daeb77d0622d2021e
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-21 12:50:32 -0500

    [add] factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const array = require('./100-data').list;
 
-const value = array.map(item => item * array.indexOf(item));
+const value = array.map((item, index) => item * index);
 console.log(array);
 console.log(value);

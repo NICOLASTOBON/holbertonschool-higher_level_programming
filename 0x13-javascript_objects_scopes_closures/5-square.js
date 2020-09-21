0643e2ba42a4b7c72a797b2891d85d94741146c3
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-21 02:35:43 -0500

    [add] class Square that inherit from class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const width = this.width;
+    const height = this.height;
+
+    for (let i = 0; i < height; i++) {
+      let val = '';
+      for (let j = 0; j < width; j++) {
+        val += 'X';
+      }
+      console.log(val);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;

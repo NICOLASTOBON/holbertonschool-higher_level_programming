abd199ce053a00ab630b1856b5e250ab925e0c6e
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-22 13:15:17 -0500

    [add]  read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const args = process.argv;
 
-fs.readFile(args[2], 'utf-8', (err, data) => {
+fs.readFile(args[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   } else {

9e6aaa0fdff9985df830382bcc5fa3f29ede70d7
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-22 14:11:54 -0500

    [add]  get status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,9 @@ const request = require('request');
 const args = process.argv;
 
 request(args[2], (err, response) => {
-  if (err) throw console.log(err);
-  console.log('code: ', response.statusCode);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
